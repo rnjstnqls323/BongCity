1c66891889030069ab2db2708ceaaf9e2c545503
Subong <rnjstnqls323@naver.com>
2025-11-11 17:47:34 +0900

    ui안올라간거 다시 업로드

---
+++ WinAPI_2504/Objects/Game/SimCity/UI/Panel.cpp
@@ -0,0 +1,21 @@
+#include "Framework.h"
+
+Panel::Panel(PanelType type)
+{
+}
+
+Panel::~Panel()
+{
+}
+
+void Panel::Update()
+{
+}
+
+void Panel::Render()
+{
+}
+
+void Panel::CreateButtons()
+{
+}
---
+++ WinAPI_2504/Objects/Game/SimCity/UI/UIManager.cpp
@@ -0,0 +1,22 @@
+#include "Framework.h"
+#include "UIManager.h"
+
+UIManager::UIManager()
+{
+}
+
+UIManager::~UIManager()
+{
+}
+
+void UIManager::Update()
+{
+}
+
+void UIManager::Render()
+{
+}
+
+void UIManager::CreatePanels()
+{
+}
